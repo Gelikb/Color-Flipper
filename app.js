6a89fdc773240ec2c97aff30a4832c6e0a534d12
Anhelina Babich <babich.gelya04@gmail.com>
2022-12-11 23:11:42 +0200

    fix app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "pink"];
 const btn = document.getElementById('btn'),
       color = document.querySelector(".color");
 
